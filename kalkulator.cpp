d53fbd718d0ddcbe57da52bff1f22ca94853af93
Bartosz Boligłowa <boliglowa.bartosz@gmail.com>
2023-05-24 13:04:22 +0200

    kalkulator z klasami

---
+++ kalkulator.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+class Kalkulator {
+
+
+
+private:
+	int a;
+	int b;
+	int wynik;
+	char znak;
+
+public:
+	Kalkulator(int a, int b, char znak) {
+		this->a = a;
+		this->b = b;
+		this->znak = znak;
+
+	}
+	void obliczenia() {
+		switch (znak) {
+		case '+':
+			wynik = a + b;
+			break;
+		case '-':
+			wynik = a - b;
+			break;
+		case '*':
+			wynik = a * b;
+			break;
+		case '/':
+			if (b == 0) {
+				return;
+			}
+			else {
+				wynik = a / b;
+				break;
+			}
+		}
+
+	}
+	int getWynik() {
+		return wynik;
+	}
+};
+
+int main() {
+	int a;
+	int b;
+	int wynik;
+	char znak;
+	cout << "podaj a:" << endl;
+	cin >> a;
+	cout << "podaj b" << endl;
+	cin >> b;
+	cout << "co chcesz zrobic" << endl;
+	cin >> znak;
+
+	Kalkulator calc = Kalkulator(a, b, znak);
+	calc.obliczenia();
+
+	cout << calc.getWynik();
+
+}
